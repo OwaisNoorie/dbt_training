1eaa26c45ce6dab38a6e4835f2666893b862b145
Owais Noorie <owaisnoorie0@gmail.com>
2026-09-10 04:36:23 +0000

    incremental data

--- models/intermediate/int_customers.sql
+++ models/intermediate/int_customers.sql
@@ -1,1 +1,8 @@
-select * from {{ref('stg_customers')}} 
+{{config(materialized='table')}}
+
+with cust as 
+(select * from{{ref('stg_customers')}}),
+
+nation as
+(select * from{{ref('stg_nations')}}),
+
